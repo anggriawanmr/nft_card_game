86d6c0dbb5391b455c529fb66f92a49b6766d51a
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-11-27 23:29:59 +0700

    Addit CreateBattle component :sparkles:

---
+++ client/src/page/CreateBattle.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { PageHOC } from '../components';
+
+const CreateBattle = () => {
+  return <div></div>;
+};
+
+export default PageHOC(
+  CreateBattle,
+  <>
+    Create <br /> a new battle
+  </>,
+  <>Create your own battle and wait for other players to join you</>
+);
