3937b0e2feab8007dd212c024c175d1b3d69ed2f
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-12-03 11:08:42 +0700

    Editing CustomInput :hammer:

--- client/src/components/CustomInput.jsx
+++ client/src/components/CustomInput.jsx
@@ -5,7 +5,21 @@ import styles from '../styles';
 const regex = /^[A-Za-z0-9]+$/;
 
 const CustomInput = ({ label, placeholder, value, handleValue }) => {
-  return <div>CustomInput</div>;
+  return (
+    <>
+      <label htmlFor="name" className={styles.label}></label>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => {
+          if (e.target.value === '' || regex.test(e.target.value))
+            handleValueChange(e.target.value);
+        }}
+        className={styles.input}
+      />
+    </>
+  );
 };
 
 export default CustomInput;
