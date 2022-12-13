ed2b1f53fde4c501c28f71000eb96163d8a11fc0
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-12-14 02:04:22 +0700

    Adding Battle component :sparkles:

---
+++ client/src/page/Battle.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import styles from '../styles';
+import { Alert } from '../components';
+import { useGlobalContext } from '../context';
+import {
+  attack,
+  attackSound,
+  defense,
+  defenseSound,
+  player01 as player01Icon,
+  player02 as player02Icon,
+} from '../assets';
+import { playAudio } from '../utils/animation.js';
+
+const Battle = () => {
+  const { contract, gameData, walletAddress, showAlert, setShowAlert } =
+    useGlobalContext();
+  const [player1, setPlayer1] = useState({});
+  const [player2, setplayer2] = useState({});
+  const { battleName } = useParams();
+  const navigate = useNavigate();
+
+  return <div>Battle</div>;
+};
+
+export default Battle;
