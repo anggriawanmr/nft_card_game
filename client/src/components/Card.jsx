0457c294fc9a391c0ef405fdce35670b7fb7c3bd
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-12-16 02:19:52 +0700

    Editing Card component :hammer:

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 
-const Card = () => {
-  return <div>Card</div>;
+import styles from '../styles';
+import { allCards } from '../assets';
+
+const generateRandomCardImage = () =>
+  allCards[Math.floor(Math.random() * (allCards.length - 1))];
+
+const img1 = generateRandomCardImage();
+const img2 = generateRandomCardImage();
+
+const Card = ({ card, title, restStyles, cardRef, playerTwo }) => {
+  return (
+    <div className={`${styles.cardContainer} ${restStyles}`}>
+      <img
+        src={playerTwo ? img2 : img1}
+        alt="card"
+        className={styles.cardImg}
+      />
+      <div
+        className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-yellow-400`}>{card.att}</p>
+      </div>
+      <div
+        className={`${styles.cardPointContainer} sm-right-[14.2%] right-[15%] ${styles.flexCenter}`}
+      >
+        <p className={`${styles.cardPoint} text-red-700`}>{card.att}</p>
+      </div>
+
+      <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
+        <p className={styles.cardText}>{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
