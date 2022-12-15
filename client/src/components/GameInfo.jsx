2d8a777f8964580577931acd7df7cad79d5e56dd
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-12-16 02:30:29 +0700

    Editing GameInfo component :hammer:

--- client/src/components/GameInfo.jsx
+++ client/src/components/GameInfo.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import CustomButton from './CustomButton';
+import { useGlobalContext } from '../context';
+import { alertIcon, gameRules } from '../assets';
+import styles from '../styles';
 
 const GameInfo = () => {
-  return <div>GameInfo</div>;
+  const { contract, gameData, setShowAleert } = useGlobalContext();
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const navigate = useNavigate();
+
+  const handleBattleExit = async () => {};
+
+  return (
+    <>
+      <div className={styles.gameInfoIconBox}>
+        <div
+          className={`${styles.gameInfoIcon} ${styles.flexCenter}`}
+          onClick={() => setToggleSidebar(true)}
+        >
+          <img src={alertIcon} alt="info" className={styles.gameInfoIconImg} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default GameInfo;
