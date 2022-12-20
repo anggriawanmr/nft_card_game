1d76931e418906af4edd0ece78577acd0ca42e31
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-12-21 00:58:26 +0700

    Adding resetParams for index context :sparkles:

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -29,6 +29,7 @@ export const GlobalContextProvider = ({ children }) => {
   });
   const [updateGameData, setUpdateGameData] = useState(0);
   const [battleGround, setBattleGround] = useState('bg-astral');
+  const [step, setStep] = useState(1);
 
   const navigate = useNavigate();
 
@@ -40,6 +41,20 @@ export const GlobalContextProvider = ({ children }) => {
     } else {
       battlegroundFromLocalStorage.setItem('battleground', battleGround);
     }
+  }, []);
+
+  // Reset web3 onboarding modal params
+  useEffect(() => {
+    const resetParams = async () => {
+      const currentStep = await GetParams();
+
+      setStep(currentStep.step);
+    };
+
+    resetParams();
+
+    window?.ethereum?.on('chainChanged', () => resetParams());
+    window?.ethereum?.on('accountChanged', () => resetParams());
   }, []);
 
   // Set the wallet address to the state
@@ -74,7 +89,7 @@ export const GlobalContextProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    if (contract) {
+    if (step !== -1 && contract) {
       createEventListener({
         navigate,
         contract,
@@ -86,7 +101,7 @@ export const GlobalContextProvider = ({ children }) => {
         setUpdateGameData,
       });
     }
-  }, [contract]);
+  }, [contract, step]);
 
   useEffect(() => {
     if (showAlert?.status) {
